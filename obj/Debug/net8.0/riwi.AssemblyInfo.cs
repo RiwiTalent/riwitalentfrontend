311b08135edc4a756467647993c425ab40f4dd2c
Fernando José Gómez Toro <fjgt2000@gmail.com>
2024-09-10 09:56:06 -0500

    feat: se cambia nombre de buttons y variables

--- obj/Debug/net8.0/riwi.AssemblyInfo.cs
+++ obj/Debug/net8.0/riwi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("riwi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e5030452dfe75e02c92850063acbd0757ea82bff")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ab04f054e43f9a6ea4fecc8201fac891ab439fd9")]
 [assembly: System.Reflection.AssemblyProductAttribute("riwi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("riwi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
