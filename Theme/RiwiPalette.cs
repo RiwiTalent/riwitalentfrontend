e9cdf7acd59799314b24f27284d01fa331c0f65c
Johan Arboleda <jhse757@gmail.com>
2024-09-18 19:44:49 -0500

    styles:Se agregan el theme por personalizado de Riwi

---
+++ Theme/RiwiPalette.cs
@@ -0,0 +1,89 @@
+namespace riwi.Theme;
+
+ public static class RiwiPalette
+    {
+        // Colores Primarios (Primary)
+        public const string Primary950 = "#0D0D10";
+        public const string Primary900 = "#17171C";
+        public const string Primary800 = "#191925";
+        public const string Primary700 = "#1D1D2E";
+        public const string Primary600 = "#191A3E";
+        public const string Primary500 = "#181E4B";
+        public const string Primary400 = "#4B64CF";
+        public const string Primary300 = "#7E98DF";
+        public const string Primary200 = "#AEC0EB";
+        public const string Primary100 = "#D3DEF4";
+        public const string Primary50 = "#E6EDF9";
+
+        // Colores Secundarios (Secondary)
+        public const string  Secondary950 = "#211650";
+        public const string  Secondary900 = "#342689";
+        public const string  Secondary800 = "#3D25AE";
+        public const string  Secondary700 = "#4B2AD8";
+        public const string  Secondary600 = "#5736F5";
+        public const string  Secondary500 = "#6B5CFF";
+        public const string  Secondary400 = "#7875FF";
+        public const string  Secondary300 = "#9CA3FF";
+        public const string  Secondary200 = "#C2C8FF";
+        public const string  Secondary100 = "#DDE1FF";
+        public const string  Secondary50 = "#ECEFFF";
+
+        // Colores de Retroalimentación (Info)
+        public const string  FeedbackInfo950 = "#60016D";
+        public const string  FeedbackInfo900 = "#941BA5";
+        public const string  FeedbackInfo800 = "#B018C9";
+        public const string  FeedbackInfo700 = "#CB26EA";
+        public const string  FeedbackInfo600 = "#D362EC";
+        public const string  FeedbackInfo500 = "#EAA2FC";
+        public const string  FeedbackInfo400 = "#EDA3FF";
+        public const string  FeedbackInfo300 = "#F1BFFF";
+        public const string  FeedbackInfo200 = "#F7DEFF";
+        public const string  FeedbackInfo100 = "#FBEFFF";
+        public const string  FeedbackInfo50 = "#FDF8FF";
+
+        // Colores de Retroalimentación (Success)
+        public const string  FeedbackSuccess950 = "#0E362B";
+        public const string  FeedbackSuccess900 = "#1C5F4C";
+        public const string  FeedbackSuccess800 = "#207359";
+        public const string  FeedbackSuccess700 = "#23926F";
+        public const string  FeedbackSuccess600 = "#2DB588";
+        public const string  FeedbackSuccess500 = "#5ACCA4";
+        public const string  FeedbackSuccess400 = "#ABD8C7";
+        public const string  FeedbackSuccess300 = "#BEE5D5";
+        public const string  FeedbackSuccess200 = "#D8F0E4";
+        public const string  FeedbackSuccess100 = "#EAF8F0";
+        public const string  FeedbackSuccess50 = "#F7FCFA";
+
+        // Colores de Retroalimentación (Danger)
+        public const string  FeedbackDanger950 = "#4A0E05";
+        public const string  FeedbackDanger900 = "#882314";
+        public const string  FeedbackDanger800 = "#A42310";
+        public const string  FeedbackDanger700 = "#C7250E";
+        public const string  FeedbackDanger600 = "#EC3116";
+        public const string  FeedbackDanger500 = "#FE654F";
+        public const string  FeedbackDanger400 = "#FF7865";
+        public const string  FeedbackDanger300 = "#FFA99D";
+        public const string  FeedbackDanger200 = "#FFCCC5";
+        public const string  FeedbackDanger100 = "#FFE4E0";
+        public const string  FeedbackDanger50 = "#FFF3F1";
+
+        // Colores de Retroalimentación (Warning)
+        public const string  FeedbackWarning950 = "#4D2E0B";
+        public const string  FeedbackWarning900 = "#87571E";
+        public const string  FeedbackWarning800 = "#A36B20";
+        public const string  FeedbackWarning700 = "#C8871E";
+        public const string  FeedbackWarning600 = "#E4B02C";
+        public const string  FeedbackWarning500 = "#E6CA52";
+        public const string  FeedbackWarning400 = "#EDE063";
+        public const string  FeedbackWarning300 = "#EEE875";
+        public const string  FeedbackWarning200 = "#F5F4AC";
+        public const string  FeedbackWarning100 = "#FAF9D8";
+        public const string  FeedbackWarning50 = "#FEFEF0";
+
+        // Colores Neutrales
+        public const string NeutralBlack = "#16161D";
+        public const string NeutralWhite = "#F9FAFC";
+        public const string NeutralGrey = "#D9D9D9";
+        public const string NeutralSoftGrey = "#F4F4F4";
+        public const string NeutralLightGrey = "#F9F9F9";
+    }
