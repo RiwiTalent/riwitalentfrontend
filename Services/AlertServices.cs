2bb0191e982b04cfbec33ddd3075f2bfbe080478
VICENTVANGOG <josemiguel200606@hmail.com>
2024-09-23 21:11:37 -0500

    feat: nuevos sweet alert para estructurarlos y sus diseños

--- Services/AlertServices.cs
+++ Services/AlertServices.cs
@@ -9,7 +9,44 @@ namespace riwi.Services
         {
             Swal = swal;
         }
-        public async Task SuccessSweetAlert()
+        
+        public async Task NewRegister()
+        {
+            var result = await Swal.FireAsync(new SweetAlertOptions
+            {
+                Icon = SweetAlertIcon.Success,
+                Title = "¿Estás seguro/a de que deseas crear este nuevo registro?",
+                Text = "Verifique que la información ingresada este correcta.",
+                ShowCancelButton = true,
+                ConfirmButtonColor = "#fff",
+                ConfirmButtonText = "No, Cancelar",
+                CancelButtonColor = "#5ACCA4",
+                CancelButtonText="<button style='color:#5ACCA4, background-color:#fff'>Si, Confirmar</button>"
+            });
+
+            if (result.IsConfirmed)
+            {
+                await Swal.FireAsync(new SweetAlertOptions
+                {
+                    Title = "Deleted!",
+                    Text = "Your file has been deleted.",
+                    Icon = SweetAlertIcon.Success
+                });
+            }
+        }
+
+        public async Task DataSuccess()
+        {
+            await Swal.FireAsync(new SweetAlertOptions
+                {   
+                    Title = "¡Los datos se han guardado exitosamente!",
+                    Icon = SweetAlertIcon.Success,
+                    ShowConfirmButton = false ,
+                    Timer = 1500
+                });
+        }
+        
+        public async Task SuccessLogin()
         {
             await Swal.FireAsync(new SweetAlertOptions
                 {   
@@ -19,5 +56,29 @@ namespace riwi.Services
                     Timer = 1500
                 });
         }
+
+        public async Task RegisterSuccess()
+        {
+            await Swal.FireAsync(new SweetAlertOptions
+                {   
+                    Title = "¡Registro se ha borrado exitosamente!",
+                    Icon = SweetAlertIcon.Success,
+                    ShowConfirmButton = false ,
+                    Timer = 1500
+                });
+        }
+
+        public async Task Warning()
+        {
+            await Swal.FireAsync(new SweetAlertOptions
+                {   
+                    Title = "¡Ups, lo sentimos ocurrió un error!",
+                    Icon = SweetAlertIcon.Warning,
+                    ShowConfirmButton = false ,
+                    Timer = 1500
+                });
+        }
+
+
     }
 }
