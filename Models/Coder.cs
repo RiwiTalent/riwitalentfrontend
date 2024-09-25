cf6c3fd63bc2148177d7ab7b07369aafdd861196
KevinDazaR <kevindaza13@gmail.com>
2024-09-25 02:25:45 -0500

    refacthor: editar coder en vista de card  coder

--- Models/Coder.cs
+++ Models/Coder.cs
@@ -17,6 +17,9 @@ namespace riwi.Models
         public string? Photo { get; set; }
         public int Age { get; set; }
         public string? Stack { get; set; }
+        public string? Status { get; set; }
+
+        // public StandarRiwi? StandarRiwi { get; set; }
         public List<Skill>? Skills { get; set; }
         public LanguageSkill? LanguageSkills { get; set; }
     }
