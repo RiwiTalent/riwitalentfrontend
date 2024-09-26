c12bfeb9d436f50dd8ae0d836c03c05e2200c597
KevinDazaR <kevindaza13@gmail.com>
2024-09-26 14:45:37 -0500

    fix: respaldo de login external

--- Services/CoderService.cs
+++ Services/CoderService.cs
@@ -17,12 +17,12 @@ namespace riwi.Services
         // Método para obtener una lista de coders desde la API
         public async Task<List<Coder>> GetCodersAsync()
         {
-            return await _httpClient.GetFromJsonAsync<List<Coder>>("https://backend-riwitalent.onrender.com/riwitalent/coders");
+            return await _httpClient.GetFromJsonAsync<List<Coder>>("https://backend-riwitalent-9pv2.onrender.com/riwitalent/coders");
         }
 
          public async Task<bool> UpdateCoderAsync(Coder coder)
         {
-            var url = $"http://localhost:5113/riwitalent/updatecoder?Id={coder.Id}&FirstName={coder.FirstName}&SecondName={coder.SecondName}&FirstLastName={coder.FirstLastName}&SecondLastName={coder.SecondLastName}&Email={coder.Email}&Age={coder.Age}";
+            var url = $"https://backend-riwitalent-9pv2.onrender.com/riwitalent/updatecoder?Id={coder.Id}&FirstName={coder.FirstName}&SecondName={coder.SecondName}&FirstLastName={coder.FirstLastName}&SecondLastName={coder.SecondLastName}&Email={coder.Email}&Age={coder.Age}";
             
             var response = await _httpClient.PutAsync(url, null); // Enviar como PUT sin cuerpo
             return response.IsSuccessStatusCode;
