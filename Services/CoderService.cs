e4ef509575d7a098be3ca1127dca7fb5bd0a1493
Johan Arboleda <jhse757@gmail.com>
2024-09-26 10:19:20 -0500

    Optimización del componente Navbar, Ademas de inclusión de avatars de la tabla de User y groups

--- Services/CoderService.cs
+++ Services/CoderService.cs
@@ -1,21 +1,20 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Net.Http.Json;
-using System.Threading.Tasks;
 using riwi.Models;
 
 namespace riwi.Services
 {
+    // Servicio para interactuar con la API de coders
     public class CoderService
     {
         private readonly HttpClient _httpClient;
+
+        // Inyección de HttpClient para realizar peticiones HTTP
         public CoderService(HttpClient httpClient)
         {
             _httpClient = httpClient;
         } 
 
-        //we realize the petition
+        // Método para obtener una lista de coders desde la API
         public async Task<List<Coder>> GetCodersAsync()
         {
             return await _httpClient.GetFromJsonAsync<List<Coder>>("https://backend-riwitalent.onrender.com/riwitalent/coders");
--- Services/GroupsServices.cs
+++ Services/GroupsServices.cs
@@ -1,19 +1,23 @@
 using System.Net.Http.Json;
 using riwi.Models;
 
-namespace riwi.Services;
-
-public class GroupsServices
+namespace riwi.Services
 {
-    private readonly HttpClient _client;
-    
-    public GroupsServices(HttpClient client)
+    // Servicio para interactuar con la API de grupos
+    public class GroupsServices
     {
-        _client = client;
-    }
+        private readonly HttpClient _client;
 
-    public async Task<List<Group>> GetGroupsAsync()
-    {
-        return await _client.GetFromJsonAsync<List<Group>>("http://localhost:5113/riwitalent/groups");
+        // Inyección de HttpClient para realizar peticiones HTTP
+        public GroupsServices(HttpClient client)
+        {
+            _client = client;
+        }
+
+        // Método para obtener una lista de coders desde la API
+        public async Task<List<Group>> GetGroupsAsync()
+        {
+            return await _client.GetFromJsonAsync<List<Group>>("http://localhost:5113/riwitalent/groups");
+        }
     }
 }
