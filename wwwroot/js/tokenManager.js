d2b86089cd073a15783ffbeef19a8f710508df7a
SebitasDev <sramirezg13@eafit.edu.co>
2024-10-23 17:09:59 -0500

    feat: integracion inicio sesion

--- wwwroot/js/tokenManager.js
+++ wwwroot/js/tokenManager.js
@@ -12,10 +12,6 @@ window.getCookie = function(name){
     if (parts.length === 2) return parts.pop().split(';').shift();
 }
 
-// Función para borrar el token de las cookies
-function deleteTokenFromCookies() {
-    document.cookie = "authToken=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
-}
 
 //Funcion para crear una cookie de auth externo
 window.setCookieExternal = function (){
@@ -27,4 +23,10 @@ window.GetCookieExternal = function (){
     const value = `; ${document.cookie}`;
     const parts = value.split(`; Auth=`);
     if (parts.length === 2) return parts.pop().split(';').shift();
+}
+
+// Función para borrar el token de las cookies
+function deleteTokenFromCookies() {
+    document.cookie = "authToken=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+    document.cookie = "Auth=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
 }
